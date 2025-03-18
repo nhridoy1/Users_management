72e6a1ffa17521221a0ee25d3df361f3d9f59edd
Nur Hossain Ridoy <ridoydeveloper@gmail.com>
2025-03-18 16:25:56 +0600

    report print done

--- Users/ReportViewer.aspx.cs
+++ Users/ReportViewer.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using Microsoft.Reporting.WebForms;
 using ReportService.Model;
 
@@ -22,7 +18,7 @@ namespace Users
 
         private void LoadReport()
         {
-
+            // Fetch Data
             DataTable dt = DatabaseHelper.ExecuteProcedureWithData("GetUsers", null);
             List<UserModel> users = new List<UserModel>();
 
@@ -38,10 +34,37 @@ namespace Users
                 });
             }
 
+            // Clear existing report data
             ReportViewer1.LocalReport.DataSources.Clear();
+
+            // Add dataset to ReportViewer
             ReportDataSource rds = new ReportDataSource("DataSet1", users);
             ReportViewer1.LocalReport.DataSources.Add(rds);
+
+            // Set Report Path
             ReportViewer1.LocalReport.ReportPath = @"C:\Users\Administrator\source\repos\Users\ReportService\Reports\UserReport.rdlc";
+
+            TimeZoneInfo bdTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Bangladesh Standard Time");
+
+            // Convert UTC time to Bangladesh time
+            DateTime bdTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, bdTimeZone);
+
+            // Convert to a valid format as a string
+            string formattedBdTime = bdTime.ToString("yyyy-MM-dd"); // or "dd-MM-yyyy" based on your RDLC format
+
+
+
+            // Define Report Parameters
+            ReportParameter[] parameters = new ReportParameter[]
+            {
+                new ReportParameter("CompanyTitle", "My Company Name"),
+                new ReportParameter("CurrentDate", formattedBdTime, true)
+        };
+
+            // Set Parameters in ReportViewer
+            ReportViewer1.LocalReport.SetParameters(parameters);
+
+            // Refresh Report
             ReportViewer1.LocalReport.Refresh();
         }
     }
